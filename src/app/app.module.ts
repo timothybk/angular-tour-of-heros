30b2ab81ae3f4fc1d1cc141e4b264d244a9310ad
Timothy BucklandKerr <timbk86@gmail.com>
2017-06-30 11:32:05 +0000

    Finish Select a dashboard hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent} from './heroes.component';
+import {HeroService} from './hero.service';
+import {DashboardComponent} from './dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+  ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [HeroService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
+
+
