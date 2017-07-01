d9acf03bb555506add346dd438e82e7e6c776adc
Tim Buckland-Kerr <timbk86@gmail.com>
2017-07-02 06:05:05 +1000

    at http promise pt6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {HeroDetailComponent} from './hero-detail.component';
@@ -10,6 +11,9 @@ import {HeroService} from './hero.service';
 import {DashboardComponent} from './dashboard.component';
 
 import {AppRoutingModule} from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import {InMemoryDataService} from './in-memory-data.service';
 
 
 @NgModule({
@@ -22,7 +26,9 @@ import {AppRoutingModule} from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [HeroService],
   bootstrap: [AppComponent],
